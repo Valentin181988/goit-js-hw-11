37fd7643ac11978792248a91835c45cc86f4d0d4
Valentin Sachenko <valentin181988@gmail.com>
2021-11-18 19:23:48 +0300

    css

--- src/index.js
+++ src/index.js
@@ -41,26 +41,26 @@ function getPhotos({ name, pageNumber}) {
 
 function renderImages(image) {
     const markup = `<div class="photo-card">
-                      <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class="img"/></a>
-                      <div class="info">
-                        <p class="info-item">
-                          <b>Likes</b>
-                          <br><span>${image.likes}</span>
-                        </p>
-                        <p class="info-item">
-                          <b>Views</b>
-                          <br><span>${image.views}</span>
-                        </p>
-                        <p class="info-item">
-                          <b>Comments</b>
-                          <br><span>${image.comments}</span>
-                        </p>
-                        <p class="info-item">
-                          <b>Downloads</b>
-                          <br><span>${image.downloads}</span>
-                        </p>
-                      </div>
-                    </div>`
+                        <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class="img"/></a>
+                         <div class="info">
+                           <p class="info-item">
+                             <b>Likes</b>
+                             <br><span>${image.likes}</span>
+                           </p>
+                           <p class="info-item">
+                             <b>Views</b>
+                             <br><span>${image.views}</span>
+                           </p>
+                           <p class="info-item">
+                             <b>Comments</b>
+                             <br><span>${image.comments}</span>
+                           </p>
+                           <p class="info-item">
+                             <b>Downloads</b>
+                             <br><span>${image.downloads}</span>
+                           </p>
+                         </div>
+                   </div>`
     gallery.insertAdjacentHTML('beforeend', markup);
 }
 
