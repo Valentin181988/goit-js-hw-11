c3e7bae786dfdfc5a9ea952d8d43adaebc9395e9
Valentin Sachenko <valentin181988@gmail.com>
2021-11-23 19:23:02 +0300

    last

--- src/get-api.js
+++ src/get-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/?key=24403049-2d622057a7d1ef54c20b3a063';
 
 async function getPhotos({ name, pageNumber }) {
-    return await axios.get(`${BASE_URL}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=10`);
+    return await axios.get(`${BASE_URL}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=40`);
 }
 
 export default { getPhotos }
